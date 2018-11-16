a35bcb16d30fb528b81f7c7a6f16cea0ad5dbc68
Waqar-107 <waqar.hassan866@gmail.com>
2018-11-16 12:16:01 +0600

    dfs implemented, replaced boat capacity from constant to k

--- Missionary and Cannibal Problem/1505107.py
+++ Missionary and Cannibal Problem/1505107.py
@@ -2,7 +2,7 @@
 
 import time
 
-boat_capacity = 2
+boat_capacity = None
 missionary = None
 cannibal = None
 
@@ -10,7 +10,6 @@ ans_found = False
 timeout = False
 
 start_time = 0
-elapsed_time = 0
 
 goal = None
 
@@ -18,7 +17,11 @@ goal = None
 time_threshold = 60
 size_threshold = int(1e8)  # max size of nodes
 
-expanded = set()
+closedList = set()
+openList = list()
+
+dx = []
+dy = []
 
 
 class state:
@@ -58,29 +61,26 @@ class state:
 def bfs():
     # d=0 for left->right, d=1 for right->left
     current_state = state(missionary, cannibal, 0, 0, 0)
-    q = list()
-    q.append(current_state)
+    openList.append(current_state)
 
     global start_time
+    global ans_found
+    global goal
+
     start_time = time.time()
 
-    while q:
-        current_state = q.pop(0)
-        expanded.add(current_state)
+    while openList:
+        current_state = openList.pop(0)
+        closedList.add(current_state)
 
         if current_state.goalReached():
-            global ans_found
             ans_found = True
-
-            global goal
             goal = current_state
-
             break
 
-        if len(expanded) >= size_threshold:
+        if len(closedList) >= size_threshold:
             break
 
-        global elapsed_time
         elapsed_time = time.time() - start_time
 
         if elapsed_time >= time_threshold:
@@ -88,47 +88,129 @@ def bfs():
             timeout = True
             break
 
-        dx = [2, 0, 1, 1, 0]
-        dy = [0, 2, 1, 0, 1]
-
         if current_state.d == 0:
-            for i in range(5):
+            for i in range(len(dx)):
                 if current_state.ml - dx[i] >= 0 and current_state.cl - dy[i] >= 0:
                     new_state = state(current_state.ml - dx[i], current_state.cl - dy[i], current_state.mr + dx[i],
                                       current_state.cr + dy[i], 1)
 
-                    if (new_state.isValid()) and (new_state not in expanded) and (new_state not in q):
-                        q.append(new_state)
+                    if (new_state.isValid()) and (new_state not in closedList) and (new_state not in openList):
+                        openList.append(new_state)
                         new_state.parent = current_state
 
         else:
-            for i in range(5):
+            for i in range(len(dx)):
                 if current_state.mr - dx[i] >= 0 and current_state.cr - dy[i] >= 0:
                     new_state = state(current_state.ml + dx[i], current_state.cl + dy[i], current_state.mr - dx[i],
                                       current_state.cr - dy[i], 0)
 
-                    if (new_state.isValid()) and (new_state not in expanded) and (new_state not in q):
-                        q.append(new_state)
+                    if (new_state.isValid()) and (new_state not in closedList) and (new_state not in openList):
+                        openList.append(new_state)
                         new_state.parent = current_state
+
+
+def dfs(current_state):
+    global ans_found
+    global goal
+    global timeout
+
+    # if goal reached
+    if current_state.goalReached():
+        ans_found = True
+        goal = current_state
+        return
+
+    elapsed_time = time.time() - start_time
+    if elapsed_time >= time_threshold:
+        return
+
+    if len(closedList) >= size_threshold:
+        return
+
+    closedList.add(current_state)
+
+    if current_state.d == 0:
+        for i in range(len(dx)):
+            if timeout or ans_found:
+                break
+
+            if current_state.ml - dx[i] >= 0 and current_state.cl - dy[i] >= 0:
+                new_state = state(current_state.ml - dx[i], current_state.cl - dy[i], current_state.mr + dx[i],
+                                  current_state.cr + dy[i], 1)
+
+                if (new_state.isValid()) and (new_state not in closedList):
+                    new_state.parent = current_state
+                    dfs(new_state)
+
+    else:
+        for i in range(len(dx)):
+            if timeout or ans_found:
+                break
+
+            if current_state.mr - dx[i] >= 0 and current_state.cr - dy[i] >= 0:
+                new_state = state(current_state.ml + dx[i], current_state.cl + dy[i], current_state.mr - dx[i],
+                                  current_state.cr - dy[i], 0)
+
+                if (new_state.isValid()) and (new_state not in closedList):
+                    new_state.parent = current_state
+                    dfs(new_state)
 
 
 def solve():
     global ans_found
     global timeout
     global start_time
-    global elapsed_time
     global goal
 
+    path = []
+    # --------------------------------------------------------------------------
+    # bfs
     ans_found = False
     timeout = False
 
     bfs()
 
     elapsed_time = time.time() - start_time
-    print("time taken by bfs:", elapsed_time, "nodes expanded: ", len(expanded))
+    print("time taken by bfs:", elapsed_time, " seconds. nodes expanded: ", len(closedList))
     if ans_found:
         print("solution found")
-        path = []
+        current_state = goal
+
+        while current_state:
+            path.append(current_state)
+            current_state = current_state.parent
+
+        d = 0
+        for i in range(len(path) - 1, 0, -1):
+            print(path[i].ml, path[i].cl, path[i].mr, path[i].cr, end="     ")
+
+            if d == 0:
+                print("-->", end="   ")
+            else:
+                print("<--", end="   ")
+
+            d = 1 - d
+            print(path[i - 1].ml, path[i - 1].cl, path[i - 1].mr, path[i - 1].cr)
+    else:
+        print("no solution")
+    # --------------------------------------------------------------------------
+
+    # --------------------------------------------------------------------------
+    # dfs
+    closedList.clear()
+    current_state = state(missionary, cannibal, 0, 0, 0)
+    start_time = time.time()
+    ans_found = False
+    timeout = False
+    goal = None
+
+    dfs(current_state)
+
+    elapsed_time = time.time() - start_time
+    print("time taken by dfs:", elapsed_time, "seconds. nodes expanded: ", len(closedList))
+    if ans_found:
+        print("solution found")
+        path.clear()
 
         current_state = goal
         while current_state:
@@ -144,17 +226,27 @@ def solve():
             else:
                 print("<--", end="   ")
 
-            d=1-d
+            d = 1 - d
             print(path[i - 1].ml, path[i - 1].cl, path[i - 1].mr, path[i - 1].cr)
-
-
     else:
         print("no solution")
+        # --------------------------------------------------------------------------
 
 
 if __name__ == "__main__":
-    # missionary, cannibal = map(int, input().split())
+    missionary, cannibal, boat_capacity = map(int, input().split())
+    # missionary = 3
+    # cannibal = 3
+    # boat_capacity = 2
 
-    missionary = 3
-    cannibal = 3
+    # generate the possible changes and fill up dx and dy
+    for i in range(1, boat_capacity + 1):
+        for j in range(i + 1):
+            dx.append(j)
+            dy.append(i - j)
+
     solve()
+
+'''
+https://puzzling.stackexchange.com/questions/55154/when-do-missionaries-and-cannibals-problems-have-solutions
+'''
