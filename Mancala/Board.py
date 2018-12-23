c82ab6f0c7bf88e183e31336f54b9e1f2e566824
Waqar-107 <waqar.hassan866@gmail.com>
2018-12-23 19:26:06 +0600

    bug in game update, requires minor changes

--- Mancala/Board.py
+++ Mancala/Board.py
@@ -27,7 +27,10 @@ class Board(object):
     def updateBoard(self, playerNo, binNo):
         turn = -1
         tot = self.bin[playerNo][binNo]
-        self.bin[playerNo][binNo] = 0
+        if playerNo == 1:
+            self.bin[playerNo][binNo] = 0
+        else:
+            self.bin[playerNo][6 - binNo - 1] = 0
 
         q = tot / 13
         r = tot % 13
@@ -35,39 +38,56 @@ class Board(object):
         # starting from binNo go counter-clockwise
         for i in range(6):
             self.bin[0][i] += q
-            self.bin[1][i] += 1
+            self.bin[1][i] += q
 
         self.storage[playerNo] += q
 
-        st = binNo
         if playerNo == 0:
-            # fill own bins
-            for i in range(binNo, -1, -1):
-                if r > 0:
-                    self.bin[playerNo][i] += 1
+            st1 = 6 - 1 - binNo - 1
+            st2 = 0
+            fn1 = 0
+            fn2 = 6
+            inc1 = -1
+            inc2 = 1
+        else:
+            st1 = binNo + 1
+            st2 = 5
+            fn1 = 6
+            fn2 = 0
+            inc1 = 1
+            inc2 = -1
 
-                    # if the last piece ends up in an empty slot
-                    # then we get this one as well as opponents stones in the same slot
-                    if r == 1 and self.bin[playerNo][i] == 1:
-                        self.storage[playerNo] += (self.bin[1-playerNo][i] + 1)
-                        self.bin[playerNo][i] = 0
-
-                    r -= 1
-            # fill won storage
+        # fill own bins
+        for i in range(st1, fn1, inc1):
             if r > 0:
-                self.storage[playerNo] += 1
-                if r == 1:
-                    turn = playerNo
+                self.bin[playerNo][i] += 1
+
+                # if the last piece ends up in an empty slot
+                # then we get this one as well as opponents stones in the same slot
+                if r == 1 and self.bin[playerNo][i] == 1:
+                    self.storage[playerNo] += (self.bin[1 - playerNo][i] + 1)
+                    self.bin[playerNo][i] = 0
+
                 r -= 1
 
-            # fill the others
-            for i in range(6):
-                if r > 0:
-                    self.bin[playerNo][i] += 1
-                    r -= 1
+        # fill own storage
+        if r > 0:
+            self.storage[playerNo] += 1
+            if r == 1:
+                turn = playerNo
+            r -= 1
 
+        # fill the others
+        for i in range(st2, fn2, inc2):
+            if r > 0:
+                self.bin[playerNo][i] += 1
+                r -= 1
 
+        # if turn is not -1 then the current player has been given chance again
+        if turn == -1:
+            turn = 1 - playerNo
 
+        return turn
 
     def gameOver(self):
         if self.storage[0] + self.storage[1] == initialStone * 6 * 2:
--- Mancala/Main.py
+++ Mancala/Main.py
@@ -5,18 +5,14 @@ board = Board()
 p1 = Player(0,2)
 p2 = Player(1,2)
 
-while True:
-    # player1
-    if board.gameOver():
-        break
+turn = 0
+while board.gameOver() == False:
+    board.printBoard()
+    print("player -",turn+1,"\'s turn")
 
-    print("player1's turn")
-    bn = p1.getNextMove(board)
+    if turn == 0:
+        bn = p1.getNextMove(board)
+    else:
+        bn = p2.getNextMove(board)
 
-    # player2
-    if board.gameOver():
-        break
-
-    print("player2's turn")
-    bn = p2.getNextMove(board)
-
+    turn = board.updateBoard(turn, bn)
